3833eba6e0972ae272cb8cd3a952d30ec96b5e9a
Sylène Manusset <sylene.manusset@gmail.com>
2019-12-31 12:50:37 +0100

    [WIP] added onChange method

--- src/SearchMovies.jsx
+++ src/SearchMovies.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { InputGroup, InputGroupAddon, Input, Button, Form } from "reactstrap";
 
 const SearchMovies = () => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleChange = event => {
+    event.preventDefault(); // To cancel the default behavior of the form submit
+    setSearchValue(event.target.value); // Method that changes the searchValue state as you enter a new letter
+  };
+
   return (
     <Form>
       <InputGroup>
-        <Input className="inputForm" />
+        <Input
+          className="inputForm"
+          value={searchValue} // Target
+          onChange={handleChange}
+        />
         <InputGroupAddon addonType="append">
           <Button type="submit" color="info">
             Search
